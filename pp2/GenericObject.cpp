bb962bc3e6ac3d1beefcb14de8b9e26f1b7a2160
Hannah Kane <hkanehaas@gmail.com>
2020-11-16 19:35:20 -0600

    Just trying to succesfully push some files.

---
+++ pp2/GenericObject.cpp
@@ -0,0 +1,41 @@
+#include "GenericObject.h"
+
+
+void GenericObject::SetName(string name)
+{
+    // TODO: Add your implementation code here.
+}
+
+
+void GenericObject::SetDescriptor(string description)
+{
+    // TODO: Add your implementation code here.
+}
+
+
+void GenericObject::SetIntVar(int objtInt)
+{
+    // TODO: Add your implementation code here.
+}
+
+
+string GenericObject::GetName()
+{
+    // TODO: Add your implementation code here.
+    return string();
+}
+
+
+string GenericObject::GetDescriptor()
+{
+    // TODO: Add your implementation code here.
+    return string();
+}
+
+
+int GenericObject::GetIntVar()
+int GenericObject::GetIntVar()
+{
+    // TODO: Add your implementation code here.
+    return 0;
+}
---
+++ pp2/GenericObject.h
@@ -0,0 +1,14 @@
+#pragma once
+class GenericObject
+class GenericObject
+{
+public:
+//	void SetName;
+	void SetName(string name);
+	void SetDescriptor(string description);
+	void SetIntVar(int objtInt);
+	string GetName();
+	string GetDescriptor();
+	int GetIntVar();
+};
+
